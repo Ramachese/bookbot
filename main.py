2487240f570095681c299dce692307c059a76ebd
Ramachese <rob.glass.42@gmail.com>
2025-12-12 12:16:13 -0500

    added report with sorted list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_word, count_characters
+from stats import count_word, count_characters, order
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,8 +10,16 @@ def main():
     book = get_book_text(fpath)
     num_words = count_word(book)
     character_dict = count_characters(book)
+    ##PRINT REPORT##
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(character_dict)
+    print("--------- Character Count -------")  
+    sorted_list = order(character_dict)
+    for i in range(0, len(sorted_list)-1):
+        print(f"{sorted_list[i]["char"]}: {sorted_list[i]["count"]}")
+    print("============= END ===============")
     return None
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def count_characters(text):
             char_dict[ch.lower()] += 1
         else: char_dict[ch.lower()] = 1
     return char_dict
+
+def sort_on(d):
+    return d["count"]
+
+def order(char_dict):
+    sorted = []
+    for char in char_dict:
+        if char.isalpha():
+            sorted.append({"char": char, "count": char_dict[char]})
+            sorted.sort(reverse=True, key=sort_on)
+    return sorted
